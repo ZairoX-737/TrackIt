a72673028747d40bb20c2272d23fe490e1a8d2b8
ZairoX <kirill5525@bk.ru>
2024-10-02 12:04:22 +0300

    Front-end | mainPage done

--- track_it_front_end/src/app/page.tsx
+++ track_it_front_end/src/app/page.tsx
@@ -2,25 +2,33 @@
 import Image from 'next/image';
 import styles from './mainpage.module.scss';
 import WLogo from '../public/logo-white.png';
+import Link from 'next/link';
 
 export default function MainPage() {
 	return (
 		<>
 			<header>
 				<nav className={styles.nav}>
-					<div>
+					<div className='flex gap-2 text-xl cursor-pointer'>
 						<Image src={WLogo} alt='Logo' width={32} height={32} />
+						<span className='self-end'>TrackIt</span>
 					</div>
 					<div className='flex gap-3 items-center'>
-						<button className={`${styles.btn} ${styles.logIn}`}>Log in</button>
-						<button className={`${styles.btn} ${styles.signIn}`}>
-							Sign Up
-						</button>
+						<Link href='/auth/login'>
+							<button className={`${styles.btn} ${styles.logIn}`}>
+								Log in
+							</button>
+						</Link>
+						<Link href='/auth/register'>
+							<button className={`${styles.btn} ${styles.signIn}`}>
+								Sign Up
+							</button>
+						</Link>
 					</div>
 				</nav>
 			</header>
-			<section className='w-screen h-[93vh]'>
-				<div className='flex flex-col justify-around items-start'>
+			<section className='w-screen h-[93vh] flex justify-between items-center'>
+				<div className='flex flex-col items-flex-start gap-3 w-fit  m-auto mt-12'>
 					<h1 className={styles.header1}>
 						Organize. Track.
 						<br />
@@ -30,12 +38,16 @@ export default function MainPage() {
 						TrackIt helps you organize tasks, track progress, and achieve
 						<br /> your goals effortlessly, whether solo or with a team.
 					</h2>
-					<button
-						className={`${styles.btn} ${styles.signIn} ${styles.btnLong}`}
-					>
-						Sign Up for free
-					</button>
+					<Link href='/auth/register'>
+						<button
+							className={`${styles.btn} ${styles.signIn} ${styles.btnLong}`}
+						>
+							Sign Up for free
+						</button>
+					</Link>
 				</div>
+
+				<div className='opacity-0 cursor-default w-[650px] h-[400px]'></div>
 			</section>
 		</>
 	);
