4576d24724919767b61c8b0dc340f64e757ce667
ZairoX <kirill5525@bk.ru>
2024-10-10 12:03:53 +0300

    Front | Auth page (WIP)

--- track_it_front_end/src/app/auth/register/page.tsx
+++ track_it_front_end/src/app/auth/register/page.tsx
@@ -1,32 +1,59 @@
 'use client';
+import Image from 'next/image';
 import styles from '../auth.module.scss';
+import WLogo from '../../../public/logo-white.png';
+import Link from 'next/link';
+import { useState } from 'react';
 
 const RegisterPage = () => {
+	const [showPassword, setShowPassword] = useState(false);
 	function handleRegister() {
 		console.log('hello');
 	}
 
 	return (
 		<div className='flex flex-col items-center gap-2'>
-			<h1 className={styles.formHeader}>Register to TaskIt</h1>
 			<form className={styles.form} id='register'>
+				<Image src={WLogo} alt='Logo' width={32} height={32} />
 				<div>
-					<span>Username</span>
+					<label>Username</label>
 					<input></input>
 				</div>
+
 				<div>
-					<span>Email</span>
+					<label>Email</label>
 					<input type='email'></input>
 				</div>
 
 				<div>
-					<span>Password</span>
-					<input type='password'></input>
+					<label>Password</label>
+					<section className={styles.showPassword}>
+						<input type={showPassword ? 'text' : 'password'}></input>
+						<button type='button' className='w-6 h-6'>
+							{showPassword ? (
+								<div className=' bg-[#ff9800] w-full h-full' />
+							) : (
+								<div className=' bg-[#00BCD4] w-full h-full' />
+							)}
+						</button>
+					</section>
 				</div>
 
-				<button className={styles.submit} onClick={handleRegister}>
-					Sign Up
-				</button>
+				<div>
+					<label>Repeat password</label>
+					<input type={showPassword ? 'text' : 'password'}></input>
+				</div>
+
+				<div className='flex justify-center items-center gap-1.5'>
+					<button className={styles.submit} onClick={handleRegister}>
+						Sign Up
+					</button>
+					<Link href={'login'}>
+						<span className=' opacity-50 font-light'>
+							Already have account?
+						</span>
+					</Link>
+				</div>
 			</form>
 		</div>
 	);
